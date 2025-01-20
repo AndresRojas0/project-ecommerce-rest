098a3ff1c8de19d29bdffc191364e2d30b356d4c
unknown <andresgustavorojas0@gmail.com>
2025-01-20 20:24:47 -0300

    add: update

--- ecommerce_rest/apps/users/api/serializers.py
+++ ecommerce_rest/apps/users/api/serializers.py
@@ -22,13 +22,17 @@ class TestUserSerializer(serializers.Serializer):
         if value == '':
             raise serializers.ValidationError('Tiene que indicar un correo')
 
-        if self.validate_name(self.context['name']) in value:
-            raise serializers.ValidationError('El email no puede contener el nombre')
-
         return value
     
     def validate(self,data):
         return data
     
     def create(self,validated_data):
-        return User.objects.create(**validated_data)
+        return self.model.create(**validated_data)
+    
+    def update(self,instance,validated_data):
+        instance.name = validated_data.get('name',instance.name)
+        instance.email = validated_data.get('email',instance.email)
+        instance.save()
+        return instance
+
