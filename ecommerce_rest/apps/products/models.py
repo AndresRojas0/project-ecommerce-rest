1bc8c0925d69d5b19536b9e8a7c9eee8a1aabc7b
unknown <andresgustavorojas0@gmail.com>
2025-01-22 10:04:40 -0300

    fix: models

--- ecommerce_rest/apps/products/models.py
+++ ecommerce_rest/apps/products/models.py
@@ -24,7 +24,6 @@ class MeasureUnit(BaseModel):
 
 class CategoryProduct(BaseModel):
     description = models.CharField('Descripción', max_length=50,blank=False,null=False,unique=True)
-    measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE)
     historical = HistoricalRecords()
 
     @property
@@ -66,6 +65,8 @@ class Product(BaseModel):
     name = models.CharField('Nombre de Producto', max_length=150, unique=True, blank=False,null=False)
     description = models.TextField('Descripción de Producto',blank=False,null=False)
     image = models.ImageField('Imagen del Producto',upload_to='products/',blank=True,null=True)
+    measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE, verbose_name='Unidad de Medida', null=True)
+    category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, verbose_name='Categoría de Producto', null=True)
     historical = HistoricalRecords()
 
     @property
