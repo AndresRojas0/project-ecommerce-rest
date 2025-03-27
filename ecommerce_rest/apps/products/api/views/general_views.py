b79d1187dafdb937f18587d23707de6ac317fad9
unknown <andresgustavorojas0@gmail.com>
2025-03-27 12:00:50 -0300

    add: viewsets routes

--- ecommerce_rest/apps/products/api/views/general_views.py
+++ ecommerce_rest/apps/products/api/views/general_views.py
@@ -1,13 +1,24 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from apps.base.api import GeneralListAPIView
+from apps.products.models import MeasureUnit
 from apps.products.api.serializers.general_serializers import MeasureUnitSerializer,CategoryProductSerializer,IndicatorSerializer
 
-class MeasureUnitViewSet(viewsets.ModelViewSet):
+class MeasureUnitViewSet(viewsets.GenericViewSet):
+    model = MeasureUnit
     serializer_class = MeasureUnitSerializer
 
-class IndicatorViewSet(viewsets.ModelViewSet):
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.filter(state = True)
+
+    def list(self,request):
+        data = self.get_queryset()
+        data = self.get_serializer(data,many = True)
+        return Response(data.data)
+
+class IndicatorViewSet(viewsets.GenericViewSet):
     serializer_class = IndicatorSerializer
 
-class CategoryProductViewSet(viewsets.ModelViewSet):
+class CategoryProductViewSet(viewsets.GenericViewSet):
     serializer_class = CategoryProductSerializer
