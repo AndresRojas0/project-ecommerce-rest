75e73bd00d8a399e71444a29f81cf911ca2b1fce
unknown <andresgustavorojas0@gmail.com>
2025-04-15 10:21:09 -0300

    add: logout

--- ecommerce_rest/ecommerce_rest/urls.py
+++ ecommerce_rest/ecommerce_rest/urls.py
@@ -21,7 +21,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from apps.users.views import Login
+from apps.users.views import Login,Logout
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -44,4 +44,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('',Login.as_view(), name = 'login'),
+    path('logout',Logout.as_view(), name = 'logout'),
 ]
