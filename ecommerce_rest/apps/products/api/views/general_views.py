1d41e0445997aa7bb08bbe3a45c9ba79fd6262df
unknown <andresgustavorojas0@gmail.com>
2025-05-13 09:17:55 -0300

    add: update & delete

--- ecommerce_rest/apps/products/api/views/general_views.py
+++ ecommerce_rest/apps/products/api/views/general_views.py
@@ -27,6 +27,9 @@ class CategoryProductViewSet(viewsets.GenericViewSet):
     def get_queryset(self):
         return self.get_serializer().Meta.model.objects.filter(state = True)
 
+    def get_object(self):
+        return self.get_serializer().Meta.model.objects.filter(id=self.kwargs['pk'],state=True)
+
     def list(self,request):
         data = self.get_queryset()
         data = self.get_serializer(data,many = True)
@@ -38,3 +41,17 @@ class CategoryProductViewSet(viewsets.GenericViewSet):
             serializer.save()
             return Response({'message':'Categoría registrada correctamente!'},status = status.HTTP_201_CREATED)
         return Response({'message':'','error':serializer.errors},status = status.HTTP_400_BAD_REQUEST)
+
+    def update(self,request,pk=None):
+        if self.get_object().exists():
+            serializer = self.serializer_class(instance=self.get_object().get(), data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'message':'Categoria actualizada correctamente!'}, status=status.HTTP_200_OK)
+        return Response({'message':'', 'error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self,request,pk=None):
+        if self.get_object().exists():
+            self.get_object().get().delete()
+            return Response({'message':'Categoría eliminada correctamente!'}, status=status.HTTP_200_OK)
+        return Response({'message':'', 'error':'Categoría no encontrada!'}, status=status.HTTP_400_BAD_REQUEST)
