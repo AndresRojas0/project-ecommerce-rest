908e5d5dd7773081c3b98656d665cbe8f475f09d
Andres Rojas <andresgustavorojas0@gmail.com>
2025-09-09 23:47:58 -0300

    add: serializer validation

--- ecommerce_rest/apps/products/api/serializers/product_serializers.py
+++ ecommerce_rest/apps/products/api/serializers/product_serializers.py
@@ -11,6 +11,26 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         exclude = ('state','created_date','modify_date','delete_data')
     
+    def validate_measure_unit(self, value):
+        if value == '' or value == None:
+            raise serializers.ValidationError('Debe ingresar una unidad de medida')
+        return value 
+    
+    def validate_category_product(self, value):
+        if value == '' or value == None:
+            raise serializers.ValidationError('Debe ingresar una categoría de producto')
+        return 
+    
+    def validate(self, data):
+        if 'measure_unit' not in data.keys():
+            raise serializers.ValidationError({
+                'measure_unit': 'Debe ingresar una unidad de medida'
+            })
+        if 'category_product' not in data.keys():
+            raise serializers.ValidationError({
+                'category_product': 'Debe ingresar una categoría de producto'
+            })
+    
     def to_representation(self,instance):
         return {
             'id': instance.id,
